40d93de6094260474da73fc9f37654db46dce78c
ramaseshan210 <ramannp210@gmail.com>
2021-09-08 23:32:07 +0530

    Project Base

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './stylesheets/main.scss'
+import 'remixicon/fonts/remixicon.css'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
